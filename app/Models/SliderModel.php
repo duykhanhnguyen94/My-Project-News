3bb889b1c4f9eb8ebcb6434916bc55152f844dd4
vuotlenchinhminh1710994 <vuotlenchinhminh171094@gmail.com>
2020-04-04 19:10:17 +0700

    select in method listItem

--- app/Models/SliderModel.php
+++ app/Models/SliderModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SliderModel extends Model
 {
-    protected $table = 'slider';
+    protected $table = 'slider as s';
     protected $primary = 'id';
     public $timestamps = false;
     const CREATED_AT = 'cteated';
@@ -15,7 +15,10 @@ class SliderModel extends Model
     public function listItems($params = null, $option){
         $result = null;
         if($option['task'] == 'admin-list-item'){
-            $result = $this->select('*')->get()->toArray();
+            $result = $this->select('s.id', 's.name', 's.description', 's.link', 's.thumb', 's.status', 's.created', 's.created_by', 's.modified', 's.modified_by')
+            ->where('id', '>', 4)
+            ->get()
+            ->toArray();
         }
 
         return $result;
