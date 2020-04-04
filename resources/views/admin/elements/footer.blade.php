79fc30aa08e051536e24e977f50b2eec76dc154b
vuotlenchinhminh1710994 <vuotlenchinhminh171094@gmail.com>
2020-04-04 15:27:23 +0700

    split interface & fix path public

---
+++ resources/views/admin/elements/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="pull-right">
+        Code by <a href="https://www.facebook.com/duykhanh171094" target="_blank">Duy Khanh</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
