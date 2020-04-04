6676cd7e076c62a0a3d7d5963e81f01f9e6d5316
vuotlenchinhminh1710994 <vuotlenchinhminh171094@gmail.com>
2020-04-04 14:47:10 +0700

    setting route for slider

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$prefixAdmin = config('myConfig.url.prefixAdmin');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => $prefixAdmin], function () {
+    $prefix = 'slider';
+    $controllerName = 'slider';
+    Route::group(['prefix' => $prefix], function () use ($controllerName) {
+        $controller = ucfirst($controllerName) . 'Controller@';
+        Route::get('/',             ['as' => $controllerName, 'uses' => $controller . 'index']);
+        Route::get('form/{?id}',    ['as' => $controllerName . 'form', 'uses' => $controller . 'form'])->where('id', '[0-9]+');
+        Route::get('delete/{id}',   ['as' => $controllerName . 'delete', 'uses' => $controller . 'delete'])->where('id', '[0-9]+');
+    });
 });
