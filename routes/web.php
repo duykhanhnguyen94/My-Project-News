63cd161c419dbeb2e1c76d4aee37aa740ff0b663
vuotlenchinhminh1710994 <vuotlenchinhminh171094@gmail.com>
2020-04-05 10:10:30 +0700

    fix as route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@ Route::group(['prefix' => $prefixAdmin], function () {
     Route::group(['prefix' => $prefix], function () use ($controllerName) {
         $controller = ucfirst($controllerName) . 'Controller@';
         Route::get('/',                            ['as' => $controllerName, 'uses' => $controller . 'index']);
-        Route::get('form/{?id}',                   ['as' => $controllerName . 'form', 'uses' => $controller . 'form'])->where('id', '[0-9]+');
-        Route::get('delete/{id}',                  ['as' => $controllerName . 'delete', 'uses' => $controller . 'delete'])->where('id', '[0-9]+');
-        Route::get('status/change-{status}/{id}',  ['as' => $controllerName . 'status', 'uses' => $controller . 'status'])->where('id', '[0-9]+');
+        Route::get('form/{?id}',                   ['as' => $controllerName . '/form', 'uses' => $controller . 'form'])->where('id', '[0-9]+');
+        Route::get('delete/{id}',                  ['as' => $controllerName . '/delete', 'uses' => $controller . 'delete'])->where('id', '[0-9]+');
+        Route::get('status/change-{status}/{id}',  ['as' => $controllerName . '/status', 'uses' => $controller . 'status'])->where('id', '[0-9]+');
     });
 
 // ============================== DASHBOARD ===========================
