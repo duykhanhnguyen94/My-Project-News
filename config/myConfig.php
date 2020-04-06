686fe3240cd013e52d28464055569a67c7059a6f
vuotlenchinhminh1710994 <vuotlenchinhminh171094@gmail.com>
2020-04-06 20:14:15 +0700

    fix position search

--- config/myConfig.php
+++ config/myConfig.php
@@ -15,7 +15,19 @@ return [
             'inactive' => ['name' => 'Chưa kích hoạt', 'class' => 'btn-success'],
             'block'    => ['name' => 'Bị khóa',        'class' => 'btn-success'],
             'default'  => ['name' => 'Chưa xác định',  'class' => 'btn-success'],
-        ]
+            'all'      => ['name' => 'Tất cả',         'class' => 'btn-success'],
+        ],
+        'search'       => [
+            'all'           => ['name'=> 'Tìm kiếm tất cả'],
+            'id'            => ['name'=> 'Tìm kiếm theo ID'],
+            'name'          => ['name'=> 'Tìm kiếm theo Tên'],
+            'username'      => ['name'=> 'Tìm kiếm theo Tài khoản'],
+            'fullname'      => ['name'=> 'Tìm kiếm theo Tên đầy đủ'],
+            'email'         => ['name'=> 'Tìm kiếm theo Email'],
+            'description'   => ['name'=> 'Tìm kiếm theo Mô tả'],
+            'link'          => ['name'=> 'Tìm kiếm theo Đường dẫn'],
+            'content'       => ['name'=> 'Tìm kiếm theo Nội dung'],
+        ],
     ],
     'config' => [
         'button' => [
@@ -27,5 +39,6 @@ return [
         'edit'   => ['class' =>'btn-warning','title'=>'Edit',  'icon'=>'fa-pencil','route-name'=>'/form'],
         'delete' => ['class' =>'btn-danger', 'title'=>'Delete','icon'=>'fa-trash', 'route-name'=>'/delete'],
         'view'   => ['class' =>'btn-info',   'title'=>'View',  'icon'=>'fa-eye',   'route-name'=>'/view'],
-    ]
+    ], 
+    
 ];
